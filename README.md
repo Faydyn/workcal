86c44c6a789e40f30b5511b7a0514de42fc86932
Nils Seitz <nils.seitz@gmail.com>
2019-12-05 17:07:50 +0100

    v1.0

--- README.md
+++ README.md
@@ -12,9 +12,11 @@ Apple .ics to csv
 * gets todays date and calcs last months range (with uneven months, leap etc)
 * filters the data, so only last month is included
 * creates Pandas DataFrame
-* DataFrame does styling for my needs
+* DataFrame does adding and splitting for my needs
+* DataFrame now contains correct content
+* converts to csv in correct format (except bottom)
 
 ### needsdoing
-* link file with calendar (Execute on every month start)**
-* make terminal find, what finder gets manually, at least by brute forcing thru all sub dirs
 * make to integration of csv to to __"official file" automated (+ pdf conv + auto mail)__**
+* 450 Border: Magic Const (pay per hour) * Arbeitszeit until now
+* shift that are after 00:00
--- code/__main__.py
+++ code/__main__.py
@@ -1,17 +1,16 @@
 import os
 import datetime
 from datetime import date
-import calendar
+import pandas as pd
 
 ENTRY_NAMES = ['work', 'Work', 'Bar']  # Names used
+MAXPERMONTH = 52
 
-workcal_path = os.path.abspath(os.path.join(os.getcwd(), '..'))
+workcal_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 rel_path = 'data/workfiles'
 workfiles_path = os.path.join(workcal_path, rel_path)
 filelist = os.listdir(workfiles_path)  # makes pathes independent of containing superfolder
 
-
-# todo: search all *.ics files via os
 
 def parse(entry):
     kind, dtime = entry.split('Europe/Berlin:')
@@ -38,6 +37,16 @@ def lastmonth():
     start = end - datetime.timedelta(days=end.day - 1)
     return start, end
 
+def add_worktime_splitshift(x):
+    return f'{x[0] + x[2]}:{(x[1] + x[3]) % 60 if (x[1] + x[3]) % 60 != 0 else "00"}'
+
+
+def getmonth_ger(monthnum):
+    names = ['Januar', 'Februar', 'Maerz', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November',
+             'Dezember']
+    numtostr = dict((k, v) for k, v in enumerate(names, 1))
+    return numtostr[monthnum]
+
 
 if __name__ == '__main__':
 
@@ -58,4 +67,69 @@ if __name__ == '__main__':
     dt_mins = convert(worktimes_all)
     dt_mins.sort()
     workedlastmonth = [dtm for dtm in dt_mins if startoflastmon <= dtm[0].date() <= endoflastmon]
-    print(workedlastmonth)
+
+    df = pd.DataFrame([list(entry) + 2 * [None] for entry in workedlastmonth],
+                      columns=['datetime_shiftstart', 'Arbeitszeit', 'Pausenzeiten von', 'Pausenzeiten bis'])
+
+    df['Arbeitsbeginn'] = [f'{dt.hour}:{dt.minute if dt.minute else "00"}' for dt in df['datetime_shiftstart']]
+    df['Tag'] = [dt.date().day for dt in df['datetime_shiftstart']]
+    df['Arbeitszeit'] = df['Arbeitszeit'].apply(
+        lambda x: f'{x // 60 if x // 60 != 0 else "00"}:{x % 60 if x % 60 != 0 else "00"}')
+    df['Arbeitsende'] = df['Arbeitsbeginn'] + ',' + df['Arbeitszeit']
+    df['Arbeitsende'] = df['Arbeitsende'].apply(lambda x: [[int(hm) for hm in t.split(':')] for t in x.split(',')])
+    df['Arbeitsende'] = df['Arbeitsende'].apply(
+        lambda x: f'{x[0][0] + x[1][0]}:{x[0][1] + x[1][1] if x[0][1] + x[1][1] != 0 else "00"}')
+
+    split_shift = list(set([day for day in df['Tag'] if list(df['Tag']).count(day) > 1]))
+
+    drops = []
+    for i in range(len(df) - 1):
+        if df.loc[i, 'Tag'] == df.loc[i + 1, 'Tag']:
+            df.loc[i, 'Pausenzeiten von'] = df.loc[i, 'Arbeitsende']
+            df.loc[i, 'Pausenzeiten bis'] = df.loc[i + 1, 'Arbeitsbeginn']
+            df.loc[i, 'Arbeitsende'] = df.loc[i + 1, 'Arbeitsende']
+            df.loc[i, 'Arbeitszeit'] = add_worktime_splitshift(
+                [int(hm) for hm in df.loc[i, 'Arbeitszeit'].split(':')] + [int(hm) for hm in
+                                                                           df.loc[i + 1, 'Arbeitszeit'].split(':')])
+            drops.append(i + 1)
+    for row in drops:
+        df = df.drop(row)
+
+    df = df.drop('datetime_shiftstart', axis=1)
+    df['Entlohnungsart'] = len(df) * [None]
+
+    c = df.columns.tolist()
+    c[0], c[1], c[2], c[3], c[4], c[5], c[6] = c[3], c[5], c[1], c[2], c[0], c[6], c[4]
+    df = df[c]
+    df.index += 1
+
+    blank = [None] * 6
+    real = pd.DataFrame([blank] * 31,
+                        columns=['Arbeitsbeginn', 'Arbeitsende', 'Pausenzeiten von', 'Pausenzeiten bis', 'Arbeitszeit',
+                                 'Entlohnungsart'])
+    real.index += 1
+
+    # entire month join
+    # todo: join to Year work list
+    for i in range(1, 32):
+        isday = df[df['Tag'] == i]
+        isday = isday.drop('Tag', axis=1)
+        if not isday.empty:
+            real.loc[i, :] = isday.iloc[0]
+    real.index.name = 'Tag'
+
+    filename = str(startoflastmon)[:-3].split('-')
+    savepath = f'Stunden_Nils_Seitz_{ filename[0]}{filename[1]}_{getmonth_ger(int(filename[1]))}.csv'
+    real.to_csv(os.path.join('/Users/nilsseitz/Desktop', savepath))
+
+    # todo: make border of 52h
+    for i in range(1, 32):
+        isday = df[df['Tag'] == i]
+        isday = isday.drop('Tag', axis=1)
+        if not isday.empty:
+            real.loc[i, :] = isday.iloc[0]
+    real.index.name = 'Tag'
+
+    filename = str(startoflastmon)[:-3].split('-')
+    savepath = f'Stunden_Nils_Seitz_{filename[0]}{filename[1]}_{getmonth_ger(int(filename[1]))}_official.csv'
+    real.to_csv(os.path.join('/Users/nilsseitz/Desktop', savepath))
