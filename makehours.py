01e75dfa71151107c409cded1c975373c413d00c
Nils Seitz <nils.seitz@gmail.com>
2020-07-01 10:52:07 +0200

    changed name of main, added shebang on top, fixed minor issues in wflow

--- makehours.py
+++ makehours.py
@@ -1,3 +1,4 @@
+#!usr/local/bin/python3
 import os
 import sys
 
@@ -9,17 +10,18 @@ from datetime import date
 
 OFFICIAL_MAX: int = 48
 tday: datetime.date = date.today()
-endoflastmon: pd.Timestamp = pd.Timestamp(tday - datetime.timedelta(days=tday.day))
-startoflastmon: pd.Timestamp = pd.Timestamp(endoflastmon - datetime.timedelta(days=endoflastmon.day - 1))
+endoflastmon: pd.Timestamp = pd.Timestamp(
+    tday - datetime.timedelta(days=tday.day))
+startoflastmon: pd.Timestamp = pd.Timestamp(
+    endoflastmon - datetime.timedelta(days=endoflastmon.day - 1))
 endoflastmon = pd.Timestamp(tday - datetime.timedelta(days=tday.day - 1))
 path: str = '/Users/nilsseitz/Desktop/work.txt'
 
-## OVERWRITE AND EXECUTE MANUALLY (NOT IN AUTOMATOR SCRIPT) FOR OTHER MONTHS
+# OVERWRITE AND EXECUTE MANUALLY (NOT IN AUTOMATOR SCRIPT) FOR OTHER MONTHS
 # year = 2020
 # month = 6
 # startoflastmon = pd.Timestamp(date(year, month, 1))
 # endoflastmon = pd.Timestamp(date(year, month + 1, 1))
-
 
 
 def delta2min(delta):
@@ -38,21 +40,25 @@ def getmonth_ger(monthnum):
 
 
 def make_dataframe(single_shifts):
-    workedlastmonth = pd.DataFrame(single_shifts, columns=['Date (Start)', 'Date (End)', 'Time (Start)', 'Time (End)'])
+    workedlastmonth = pd.DataFrame(single_shifts, columns=[
+                                   'Date (Start)', 'Date (End)', 'Time (Start)', 'Time (End)'])
     workedlastmonth['datetime_shiftstart'] = (workedlastmonth['Date (Start)'] + workedlastmonth['Time (Start)']) \
         .apply(lambda x: datetime.datetime.strptime(x, '%d.%m.%y%H:%M:%S'))
     workedlastmonth['amount_min'] = (workedlastmonth['Time (End)'].
-                                     apply(lambda x: datetime.datetime.strptime(x, '%H:%M:%S'))
+                                     apply(lambda x: datetime.datetime.strptime(
+                                         x, '%H:%M:%S'))
                                      - workedlastmonth['Time (Start)'].
                                      apply(lambda x: datetime.datetime.strptime(x, '%H:%M:%S'))). \
         apply(lambda x: delta2min(x))
-    workedlastmonth = workedlastmonth[startoflastmon <= workedlastmonth['datetime_shiftstart']]
+    workedlastmonth = workedlastmonth[startoflastmon <=
+                                      workedlastmonth['datetime_shiftstart']]
     workedlastmonth = workedlastmonth[workedlastmonth['datetime_shiftstart'] <= endoflastmon]
-    workedlastmonth = workedlastmonth.drop(['Date (Start)', 'Date (End)', 'Time (Start)', 'Time (End)'], axis=1)
+    workedlastmonth = workedlastmonth.drop(
+        ['Date (Start)', 'Date (End)', 'Time (Start)', 'Time (End)'], axis=1)
     workedlastmonth = workedlastmonth.reset_index(drop=True)
     worktime_frac_hours = workedlastmonth['amount_min'].sum() / 60
 
-    return workedlastmonth.fillna(0), int(worktime_frac_hours) if  worktime_frac_hours.is_integer() else  worktime_frac_hours
+    return workedlastmonth.fillna(0), int(worktime_frac_hours) if worktime_frac_hours.is_integer() else worktime_frac_hours
 
 
 def make_official_df(workedlastmonth):
@@ -62,22 +68,27 @@ def make_official_df(workedlastmonth):
         worktime_in_hours = sum_of_workinghours / 60
         return workedlastmonth.fillna(0), int(worktime_in_hours) if worktime_in_hours.is_integer() else worktime_in_hours
     else:
-        workedlastmonth_official = workedlastmonth[workedlastmonth['amount_min'].cumsum() < official_split]
+        workedlastmonth_official = workedlastmonth[workedlastmonth['amount_min'].cumsum(
+        ) < official_split]
         rest = official_split - workedlastmonth_official['amount_min'].sum()
-        workedlastmonth_official = workedlastmonth_official.append(workedlastmonth.loc[len(workedlastmonth_official)])
-        workedlastmonth_official.loc[len(workedlastmonth_official) - 1, 'amount_min'] = rest
+        workedlastmonth_official = workedlastmonth_official.append(
+            workedlastmonth.loc[len(workedlastmonth_official)])
+        workedlastmonth_official.loc[len(
+            workedlastmonth_official) - 1, 'amount_min'] = rest
         return workedlastmonth_official.fillna(0), OFFICIAL_MAX
 
 
 def format_dataframe(df):
     df = df.append(pd.DataFrame(np.full((0, 4), 0), columns=['Arbeitsbeginn', 'Tag', 'Arbeitszeit',
                                                              'Arbeitsende']))
-    df['Arbeitsbeginn'] = [f'{dt.hour}:{dt.minute if dt.minute != 0 else "00"}' for dt in df['datetime_shiftstart']]
+    df['Arbeitsbeginn'] = [
+        f'{dt.hour}:{dt.minute if dt.minute != 0 else "00"}' for dt in df['datetime_shiftstart']]
     df['Tag'] = [dt.date().day for dt in df['datetime_shiftstart']]
     df['Arbeitszeit'] = df['amount_min'].apply(
         lambda x: f'{int(x) // 60 if int(x) // 60 != 0 else "00"}:{int(x) % 60 if int(x) % 60 != 0 else "00"}')
     df['Arbeitsende'] = df['Arbeitsbeginn'] + ',' + df['Arbeitszeit']
-    df['Arbeitsende'] = df['Arbeitsende'].apply(lambda x: [[int(hm) for hm in t.split(':')] for t in x.split(',')])
+    df['Arbeitsende'] = df['Arbeitsende'].apply(
+        lambda x: [[int(hm) for hm in t.split(':')] for t in x.split(',')])
     df['Arbeitsende'] = df['Arbeitsende'].apply(
         lambda x: f'{(x[0][0] + x[1][0]) % 24}:{(x[0][1] + x[1][1]) % 60 if (x[0][1] + x[1][1]) % 60 != 0 else "00"}')
 
@@ -133,18 +144,21 @@ if __name__ == '__main__':
         shifts = list(f)[3:-1]  # clean file of header and last empty line
 
         shift = []
-        for line in shifts:  # split and clean single shifts, so only Date&Time are left (per shift)
+        # split and clean single shifts, so only Date&Time are left (per shift)
+        for line in shifts:
             if line == '\n':
                 single_shifts.append(shift)
                 shift = []
             elif line.startswith('Date'):
-                shift.append(line.strip('Date:\t')[:-1].split(' to '))  # cleaning unnecessary pre- and suffix
+                # cleaning unnecessary pre- and suffix
+                shift.append(line.strip('Date:\t')[:-1].split(' to '))
             elif line.startswith('Time'):
                 shift.append(line.strip('Time:\t')[:-1].split(' to '))
             else:
                 pass
 
-    single_shifts = [list(itertools.chain.from_iterable(shift)) for shift in single_shifts]
+    single_shifts = [list(itertools.chain.from_iterable(shift))
+                     for shift in single_shifts]
 
     df, month_sum = make_dataframe(single_shifts)
     df_official, month_sum_official = make_official_df(df)
@@ -175,10 +189,13 @@ if __name__ == '__main__':
             f.write(readin)
 
     df_formatted.loc['Summe', 'Entlohnungsart'] = overtime_str
-    df_formatted.to_csv(os.path.join('/Users/nilsseitz/Desktop', f'{savepath}.csv'))
+    df_formatted.to_csv(os.path.join(
+        '/Users/nilsseitz/Desktop', f'{savepath}.csv'))
 
     df_official_formatted = format_dataframe(df_official)
-    df_official_formatted = add_monthly_sum(df_official_formatted, month_sum_official)
-    df_official_formatted.to_csv(os.path.join('/Users/nilsseitz/Desktop', f'{savepath}_official.csv'))
+    df_official_formatted = add_monthly_sum(
+        df_official_formatted, month_sum_official)
+    df_official_formatted.to_csv(os.path.join(
+        '/Users/nilsseitz/Desktop', f'{savepath}_official.csv'))
 
     sys.exit()
